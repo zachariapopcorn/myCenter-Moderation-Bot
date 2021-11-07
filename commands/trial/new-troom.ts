107f33598d2416f0a0e126d4c855412f8b40eb66
zachariapopcorn <robloxandminecraft19976@gmail.com>
2021-11-07 14:42:54 -0800

    Add trial commands

--- commands/trial/new-troom.ts
+++ commands/trial/new-troom.ts
@@ -1,1 +1,21 @@
- 
+import Discord = require('discord.js');
+import Builders = require('@discordjs/builders');
+
+import * as functions from "../../utils/functions"
+
+import { Arguements } from "../../utils/classes";
+
+export let ranks = ["MP"];
+
+export async function run(interaction : Discord.CommandInteraction, client : Discord.Client, args : Arguements[]) {
+    let embed = functions.embedMaker(client.user, "Welcome to your room!", "Here, everything will remain private. We ask that you do not share any images or messages from this chat.");
+    embed.embeds[0].addField("Getting started", "Please wait for your trainer to respond! Once they have done so, you will begin. The room will be purged multiple times, so make sure to remember things!");
+    embed.embeds[0].addField("Information", "You can have the [guide](https://bit.ly/mCModHandbook) when you wish. Make sure that you remember stuff from that!");
+    embed.embeds[0].addField("AFK/GTG", "If you have to go for longer than 3 hours, please notify your trainer by pinging them.\nThis will allow us to see that you are AFK.\nWhen you are back, make sure to ping them!");
+    embed.embeds[0].addField("Good Luck", "We wish you the best of luck!");
+    return interaction.editReply(embed);
+}
+
+export let data = new Builders.SlashCommandBuilder()
+    .setName("new-troom")
+    .setDescription("Sends the trial room embed in a channel")
--- commands/trial/trialp.ts
+++ commands/trial/trialp.ts
@@ -1,1 +1,42 @@
- 
+import Discord = require('discord.js');
+import Builders = require('@discordjs/builders');
+import fs = require('then-fs');
+
+import * as functions from "../../utils/functions"
+
+import { Arguements } from "../../utils/classes";
+
+export let ranks = ["MP"];
+
+export async function run(interaction : Discord.CommandInteraction, client : Discord.Client, args : Arguements[]) {
+    let uID = functions.getArguement("id");
+    let message = await fs.readFile("./messages/trialp.txt", "utf8");
+    let roles = Array.from((await interaction.guild.roles.fetch()).values());
+    let member;
+    try {
+        member = await interaction.guild.members.fetch(uID);
+        if(member) {
+            let dmChannel = await member.createDM();
+            await dmChannel.send({content: message});
+        } else {
+            throw new Error("User unable to be fetched");
+        }
+    } catch(e) {
+        await interaction.channel.send(functions.embedMaker(interaction.user, "Error", `There was an error while trying to send this user a DM: ${e}`));
+    }
+    try {
+        await member.roles.remove(roles.find(r => r.name === "Trial Enforcement"));
+        await member.roles.add(roles.find(r => r.name === "Junior Community Enforcement"));
+        await member.roles.add(roles.find(r => r.name === "Team Member"));
+        await member.roles.add(roles.find(r => r.name === "Discord"));
+        await member.roles.add(roles.find(r => r.name === "Website"));
+    } catch(e) {
+        await interaction.channel.send(functions.embedMaker(interaction.user, "Error", `There was an error while trying to modify the user's roles: ${e}`));
+    }
+    return await interaction.editReply({content: "Interaction completed"});
+}
+
+export let data = new Builders.SlashCommandBuilder()
+    .setName("trialp")
+    .setDescription("Passes a user's trial")
+    .addStringOption(option => option.setName("id").setDescription("The id of the person to pass").setRequired(true));
