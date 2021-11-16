7ce6e10cace11a5f269403cb8f0f5298342c672a
zachariapopcorn <robloxandminecraft19976@gmail.com>
2021-11-15 16:51:10 -0800

    Remove Director check on command getactivitydata

--- commands/management/getactivitydata.ts
+++ commands/management/getactivitydata.ts
@@ -5,8 +5,6 @@ import * as functions from "../../utils/functions"
 
 import { Arguements } from "../../utils/classes";
 
-export let ranks = ["Director"];
-
 export async function run(interaction : Discord.CommandInteraction, client : Discord.Client, args : Arguements[]) {
     let data
     try {
