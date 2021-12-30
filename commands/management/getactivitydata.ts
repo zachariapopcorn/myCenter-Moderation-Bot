6ec2e36758191f9088c0846bcc1783b55e150453
zachariapopcorn <robloxandminecraft19976@gmail.com>
2021-12-29 19:16:05 -0800

    Make id argument in getactivitydata command optional and if id not supplied, get own data

--- commands/management/getactivitydata.ts
+++ commands/management/getactivitydata.ts
@@ -6,17 +6,21 @@ import * as functions from "../../utils/functions"
 import { Arguements } from "../../utils/classes";
 
 export async function run(interaction : Discord.CommandInteraction, client : Discord.Client, args : Arguements[]) {
+    let id = functions.getArguement("id");
+    if(!id) {
+        id = interaction.user.id;
+    }
     let data
     try {
-        data = await functions.getLogs(functions.getArguement("id"));
+        data = await functions.getLogs(id);
     } catch(e) {
         return await interaction.editReply(functions.embedMaker(interaction.user, "Error", `There was an error while trying to get the activity data for this staff member: ${e}`));
     }
     let totalAmountOfLogs = data.discord + data.reportedApps + data.subdomains;
-    return await interaction.editReply(functions.embedMaker(interaction.user, "Activity Data", `This staff member (<@${functions.getArguement("id")}>) has completed ${totalAmountOfLogs} logs\n\n**Discord**: ${data.discord}\n**Reported Applications**: ${data.reportedApps}\n**Subdomains**: ${data.subdomains}`));
+    return await interaction.editReply(functions.embedMaker(interaction.user, "Activity Data", `This staff member (<@${id}>) has completed ${totalAmountOfLogs} logs\n\n**Discord**: ${data.discord}\n**Reported Applications**: ${data.reportedApps}\n**Subdomains**: ${data.subdomains}`));
 }
 
 export let data = new Builders.SlashCommandBuilder()
     .setName("getactivitydata")
     .setDescription("Gets the activity data for a certain user")
-    .addStringOption(option => option.setName("id").setDescription("The staff id of the staff member that you wish to view the activity data for").setRequired(true))
+    .addStringOption(option => option.setName("id").setDescription("The staff id of the staff member that you wish to view the activity data for").setRequired(false))
